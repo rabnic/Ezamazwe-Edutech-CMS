cd193a20d6d50a83b8e02702b490356a8ef0359b
promisemagoga <tshegofatsopromise362@gmail.com>
2023-11-22 08:17:51 +0200

    Fixed the responsiveness of the components

--- src/Components/AppBarSubHeading.jsx
+++ src/Components/AppBarSubHeading.jsx
@@ -4,7 +4,7 @@ import { Typography } from '@mui/material'
 const AppBarSubHeading
   = ({ children }) => {
     return (
-      <Typography variant="h6" component="h6" sx={{ color: 'black.main', fontWeight: "400", lineHeight: "125%", letterSpacing: "0.25px", fontSize: { xs: "1rem" }, textAlign: { xs: "center", lg: "left", md: "left" } }}>
+      <Typography variant="h6" component="h6" sx={{ color: 'black.main', fontWeight: "400", lineHeight: "125%", letterSpacing: "0.25px", fontSize: { xs: "1rem" }, textAlign: { xs: "center", sm: "left", lg: "left" } }}>
         {children}
       </Typography>
     )
